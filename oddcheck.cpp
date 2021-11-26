550db0131ad10e16accde6962d4562531111cbf2
Brendon <murph4012@gmail.com>
2021-11-26 01:28:54 -0500

    Initial commit

---
+++ oddcheck.cpp
@@ -0,0 +1,13 @@
+#include "oddcheck.h"
+
+int main(void){
+    cout << "Please input a number to check whether it is even or odd." << endl;
+    cin >> num;
+
+    if(num%2 == 0){ //Divides number by 2 to see if remainder is 0 or 1
+        cout << "The number " << num << " is an even number." << endl;
+    }
+    else{
+        cout << "The number " << num << " is an odd number." << endl;
+    }
+}
